45d302c048af34c0457d1226ac392f944784f5bf
muxueqz <zhangmingyuan240@gmail.com>
2019-07-25 20:30:22 +0800

    feat: support output index page

--- build.nim
+++ build.nim
@@ -4,6 +4,7 @@ import tables
 import pegs
 import nwt, json
 import markdown
+import algorithm
 
 var templates = newNwt("templates/*.html") # we have all the templates in a folder called "templates"
 
@@ -24,7 +25,6 @@ proc parse_source(file_path: string): JsonNode =
     post = initTable[string, string]()
     matches: array[0..1, string]
     src = ""
-  # post.slug = file_meta.name
   for line in file_path.open().lines:
 
     if head and line.match(peg"\s* {\w+} \s* ':' \s* {.+}", matches):
@@ -33,6 +33,8 @@ proc parse_source(file_path: string): JsonNode =
       head = false
     if head == false:
       src.add line & "\n"
+  if post.contains"Slug":
+    post["Slug"] = file_meta.name
   post["content"] = markdown(src)
   result = %* post
 
@@ -48,12 +50,38 @@ proc write_posts(): seq[JsonNode] =
         # posts.append(post)
       result.add post
 
-# def write_index(posts: Sequence[frontmatter.Post]):
+proc write_index(posts: seq[JsonNode]) =
     # posts = sorted(posts, key=lambda post: post['date'], reverse=True)
-    # path = pathlib.Path("./docs/index.html")
-    # template = jinja_env.get_template('index.html')
-    # rendered = template.render(posts=posts)
-    # path.write_text(rendered)
+    var
+      seq_post : seq[string]
+      p: string
+      post_array: seq[JsonNode]
+      post_tables = initOrderedTable[string, JsonNode]()
+    for post in posts:
+      post_tables.add(post["Date"].getStr, post)
+    post_tables.sort(system.cmp, order = SortOrder.Descending)
+
+    for key, post in post_tables:
+      p = """
+    <a href="/$1.html">
+      <dt>$2</dt>
+      <dd>
+        <time>$3</time>
+      </dd>
+    </a>
+    """ % [
+          post["Slug"].getStr,
+          post["Title"].getStr,
+          post["Date"].getStr,
+          ]
+      seq_post.add p
+
+    var index_post = %* {
+        "content": seq_post.join("\n")
+      }
+    var content = templates.renderTemplate("index.html", index_post)
+    
+    writeFile("public/" & "index.html", content)
 #
 #
 # def write_rss(posts: Sequence[frontmatter.Post]):
@@ -69,8 +97,8 @@ proc main()=
   var
     posts = write_posts()
   # echo $posts
-    # write_index(posts)
-    # write_rss(posts)
+  write_index(posts)
+  # write_rss(posts)
 
 
 main()
